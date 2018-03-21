50d8c2c9f22e02ca62f7317b4e966543d355073e
Ivan <wu0719900522@gmail.com>
2018-03-21 18:28:56 +1100

    Revert "as_json in post model"
    This reverts commit 7bdda5e36f73c5107ce5b659595d802c0b6af529.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,19 +3,4 @@ class Post < ApplicationRecord
 
   has_many :photos, dependent: :destroy
   accepts_nested_attributes_for :photos, allow_destroy: true
-
-  def as_json(_opts = {})
-    {
-        id: id,
-        caption: caption,
-        errors: errors,
-        photo_images: photos.map do |x|
-          {
-              url: x.image.url.absolute_url,
-              name: x.image_file_name,
-              id: x.id
-          }
-        end
-    }
-  end
 end
