b3edc018f7920b706e43a81dc4860709355a3f43
iVANWU0522 <wu0719900522@gmail.com>
2018-03-28 18:58:15 +1100

    ...

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,7 +27,7 @@ class PostsController < ApplicationController
   # GET /posts/new
   # GET /posts/new.json
   def new
-    @post = Post.new
+    @post = current_user.posts.build
 
     respond_to do |format|
       format.html # new.html.erb
@@ -43,6 +43,8 @@ class PostsController < ApplicationController
   # POST /posts
   # POST /posts.json
   def create
+    @post = current_user.posts.build(post_params)
+
     respond_to do |format|
       if @post.save
 
