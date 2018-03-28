1078cd0e741dfc4a5fbbeb805dc130bcedbc76fa
iVANWU0522 <wu0719900522@gmail.com>
2018-03-28 16:58:09 +1100

    users sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
+  validates :user_name, presence: true, length: { minimum: 2, maximum: 16 }
+  validates_format_of :user_name, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :confirmable, :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:login]
 end
