3d5556e61ec1e354ce8eb950e09034e287ee2dd6
Ivan <wu0719900522@gmail.com>
2018-04-03 08:58:17 +1000

    Revert "users sign up"
    This reverts commit 1078cd0e741dfc4a5fbbeb805dc130bcedbc76fa.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180328055649) do
+ActiveRecord::Schema.define(version: 20180328043643) do
 
   create_table "photos", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "post_id"
@@ -42,13 +42,8 @@ ActiveRecord::Schema.define(version: 20180328055649) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "user_name"
-    t.string "first_name"
-    t.string "last_name"
-    t.boolean "is_female", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["user_name"], name: "index_users_on_user_name", unique: true
   end
 
 end
