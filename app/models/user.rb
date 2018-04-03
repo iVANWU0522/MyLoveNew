a80881977ca439f9ec4ce5a71cf472738a069cd7
iVANWU0522 <wu0719900522@gmail.com>
2018-04-03 11:50:36 +1000

    add username firstname and lastname validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Validation
+  validates :user_name, presence: true, length: { minimum: 4, maximum: 16 }
+  validates :first_name, presence: true, length: { minimum: 2, maximum: 16 }
+  validates :last_name, presence: true, length: { minimum: 2, maximum: 16 }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
