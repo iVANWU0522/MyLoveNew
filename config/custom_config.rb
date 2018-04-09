cef49377c447e13d0de059a291082973d73228be
iVANWU0522 <wu0719900522@gmail.com>
2018-04-09 18:29:17 +1000

    smtp setting in development

--- config/custom_config.rb
+++ config/custom_config.rb
@@ -1,3 +1,27 @@
 Rails.application.configure do
 
+  config.x.local_domain               = 'localhost:3000'
+  config.x.domain                     = 'xiangyuxin.com'
+  ##############################################################################
+  # CLIENT SMTP SETTINGS
+  ##############################################################################
+
+  config.x.smtp_raise_delivery_errors = true #false in production
+  config.x.smtp_delivery              = false #true in production only
+  config.x.smtp_port                  = 587
+  config.x.smtp_address               = 'smtp.gmail.com'
+  config.x.smtp_authentication        = 'login'
+  config.x.smtp_user_name             = 'ivan.wu7887@gmail.com'
+  config.x.smtp_password              = ENV['MAIL_PASSWORD']
+  config.x.smtp_enable_starttls_auto  = true
+
+
+  ##############################################################################
+  # CLIENT EMAIL SETTINGS
+  ##############################################################################
+
+  config.x.email_contact   = 'wu0719900522@gmail.com'
+  config.x.email_itsupport = 'wu0719900522@gmail.com'
+  config.x.email_safety    = 'wu0719900522@gmail.com'
+
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,9 +26,6 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
@@ -52,9 +49,22 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
+  config.action_mailer.perform_deliveries = true
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = true
   #default url options
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # showing mail in browser
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+      user_name:            Rails.application.config.x.smtp_user_name,
+      password:             Rails.application.config.x.smtp_password ,
+      domain:               localhost:3000,
+      address:              Rails.application.config.x.smtp_address,
+      port:                 Rails.application.config.x.smtp_port,
+      authentication:       Rails.application.config.x.smtp_authentication,
+      enable_starttls_auto: Rails.application.config.x.smtp_enable_starttls_auto
+  }
 end
