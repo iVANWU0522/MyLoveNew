83ad13763bdd3ceec6c9b944561e0c14179836ae
iVANWU0522 <wu0719900522@gmail.com>
2018-04-11 18:22:54 +1000

    add user_id to posts model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ApplicationRecord
+  validates :user_id, presence: true
   validates :caption, presence: true
 
+  belongs_to :user
   has_many :photos, dependent: :destroy
   accepts_nested_attributes_for :photos, allow_destroy: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   validates :user_name, presence: true, length: { minimum: 4, maximum: 16 }
   validates :first_name, presence: true, length: { minimum: 2, maximum: 16 }
   validates :last_name, presence: true, length: { minimum: 2, maximum: 16 }
+
+  has_many :posts, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
