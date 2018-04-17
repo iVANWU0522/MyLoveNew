9a79c481e17d948354205937115ccaae5a4590fc
iVANWU0522 <wu0719900522@gmail.com>
2018-04-17 13:35:49 +1000

    posts paginating with ajax and posts with one image and mutiple image display issue

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,11 +6,11 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.all.order('created_at DESC').page params[:page]
 
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @posts }
+      format.js
     end
   end
 
